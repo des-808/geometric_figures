5544f5bd87ffb62fd843d3cdb275902c2f6a326e
des-808 <des-808@inbox.ru>
2022-07-17 12:39:28 +0300

    xx

--- geometric_figures/geometric figures_main.cpp
+++ geometric_figures/geometric figures_main.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 class Geometric_figures {
-protected:
-	double r{};
-	double height{};   // высота
-	double width{};	// ширина
+//protected:
 public:
+	double r;
+	double height;   // высота
+	double width;	// ширина
+
 	const double& get_r()const { return r; }
 	const double& get_height()const { return height; }
 	const double& get_width()const { return width; }
@@ -16,136 +17,144 @@ public:
 	void set_r(double r){ this->r = r; }
 	void set_height(double height){ this->height = height; }
 	void set_width(double width){ this->width = width; }
+	void set_two_param(double height, double width) { this->height = height; this->width = width;}
 
-	Geometric_figures() {
+	/*Geometric_figures() {
+		this->r = 0;
+		this->height = 0;
+		this->width = 0;
 		cout << "Geometric_figuresConstructor0param:\t" << this << endl;
-	}
-	Geometric_figures(const double& r) {
+	}*/
+	Geometric_figures( double r) {
 		set_r(r);
 		cout << "Geometric_figuresConstructor1param:\t" << this << endl;
 	}
-	Geometric_figures(const double& height, const double& width) {
+	Geometric_figures(double height,double width) {
 		set_height(height);
 		set_width(width);
 		cout << "Geometric_figuresConstructor2param:\t" << this << endl;
 	}
+	Geometric_figures(double r, double height, double width) {
+		set_r(r);
+		set_height(height);
+		set_width(width);
+		cout << "Geometric_figuresConstructor3param:\t" << this << endl;
+	}
 	  
 	virtual ~Geometric_figures(){cout << "Geometric_figuresDEstructor:\t" << this << endl;}
-	//virtual double perimeter() = 0;//периметр
-	//virtual double square() = 0;	//площадь
+	virtual double perimeter() = 0;//периметр
+	virtual double square() = 0;	//площадь
 
-	virtual double perimeter(double r) = 0;//периметр
-	virtual double square(double r) = 0;	//площадь
+	//virtual double perimeter(double r) = 0;//периметр
+	//virtual double square(double r) = 0;	//площадь
 	 
 	//virtual double perimeter(double r, double width) = 0;//периметр
-	//virtual double square(double r, double width) = 0;	//площадь
+	//virtual double square(double r, double width) = 0;   //площадь
 };
 
-class Square_figure : Geometric_figures {//квадрат
-	double width{};   // ширина
+class Square_figure : public  Geometric_figures {//квадрат
+	//double width{};   // ширина
 public:
 	const double get_width()const { return width; }
-	void set_width(const double& width) { 
-		this->width = width+1e9;
-	}
-	Square_figure() {
-		//set_width(width);
+	void set_width(const double& width) { this->width = width;}
+	/*Square_figure() {
+		set_width(width);
 		cout << "Square_figureConstructorPoUmolchaniyu:\t" << this << endl;
-	}
-	Square_figure(const double& width) :Geometric_figures(width) {
+	}*/
+	Square_figure() :Geometric_figures(width) {
 		set_width(width);
 		cout << "Square_figureConstructor3:\t" << this << endl;
 	}
 
 	~Square_figure(){ cout << "Square_figureDEstructor:\t" << this << endl; }
-	double perimeter( double width) {//периметр
+	double perimeter( ) {//периметр
 		return width * 4;
 	}
 
-	double square( double width) {	//площадь
+	double square() {	//площадь
 		return width * width;
 	}
 
 };
 
-class Rectangle : Geometric_figures {//прямоугольник
+class Rectangle : public Geometric_figures {//прямоугольник
 
-	double height{};   // высота
-	double width{};	// ширина
+	//double height{};   // высота
+	//double width{};	// ширина
 public:
 	const double& get_height()const { return height; }
 	const double& get_width()const { return width; }
-	void set_height(const double& height) { this->height = height + 1e9; }
-	void set_width(const double& width) { this->width = width + 1e9; }
-
-	Rectangle() {
+	void set_height(const double& height) { this->height = height; }
+	void set_width(const double& width) { this->width = width; }
+	void set_two_param(double height, double width) { this->height = height; this->width = width; }
+	/*Rectangle() {
 		cout << "REctangleConstructor0param:\t" << this << endl;
-	}
-	Rectangle(const double& height, const double& weight):Geometric_figures(height, width) {
-		set_height(height + 1e9);
-		set_width(width + 1e9);
+	}*/
+	Rectangle():Geometric_figures(height, width) {
+		set_height(height);
+		set_width(width);
 		cout << "REctangleConstructor2param:\t" << this << endl;
 	}
 	~Rectangle() { cout << "REctangleDEstructor:\t" << this << endl; }
-	double perimeter(double height, double width) {//периметр
+	double perimeter() {//периметр
 		return  2 *(height + width);
 	}
 
-	double square(double height, double width) {	//площадь
+	double square() {	//площадь
 		return height * width;
 	}
 	
 };
 
-class Circle : Geometric_figures {//круг
-	double r{};
+class Circle :public  Geometric_figures {//круг
+	//double r{};
 public:
 	const double& get_r()const { return r; }
-	void set_r(const double& r) { this->r = r + 1e9; }
+	void set_r(const double& r) { this->r = r; }
 
 
-	Circle() {
-		//set_r(0);
-	}
-	Circle(double r) :Geometric_figures(r) {
+	//Circle() {
+	//	//set_r(0);
+	//}
+	Circle() :Geometric_figures(r) {
 		set_r(r);
 	}
 
 	~Circle(){ cout << "CirkleleDEstructor:\t" << this << endl; }
-	double perimeter(double r) {//периметр
+	double perimeter() {//периметр
 		return 2 * r * M_PI;
 	}
-	double square(double r) {	//площадь
+	double square() {	//площадь
 		return r * r * M_PI;
 	}
 };
 
-class Triangle : Geometric_figures {//треугольник
-	double x{};
+class Triangle :public  Geometric_figures {//треугольник
+	/*double x{};
 	double y{};
-	double z{};
+	double z{};*/
 public:
-	const double& get_x()const { return x; }
-	const double& get_y()const { return y; }
-	const double& get_z()const { return z; }
-	void set_x(const double& x) { this->x = x + 1e9; }
-	void set_y(const double& y) { this->y = y + 1e9; }
-	void set_z(const double& z) { this->z = z + 1e9; }
-	Triangle() {
+	const double& get_x()const { return r; }
+	const double& get_y()const { return height; }
+	const double& get_z()const { return width; }
+	void set_x(const double& r) { this->r = r; }
+	void set_y(const double& height) { this->height = height; }
+	void set_z(const double& width) { this->width = width; }
+	/*Triangle() {
 		cout << "TriangleConstructor0:\t" << this << endl;
-	}
-	Triangle(const double& x,const double& y,const double& z) {
-		set_x(x + 1e9);
-		set_y(y + 1e9);
-		set_z(z + 1e9);
+	}*/
+	Triangle():Geometric_figures(r,height,width) {
+		set_x(r);
+		set_y(height);
+		set_z(width);
 		cout << "TriangleConstructor3:\t" << this << endl;
 	}
 	~Triangle() { cout << "TriangleDestructor:\t" << this << endl; }
 
-	void  info()const {
-		//Geometric_figures::info();
-		cout << "x= " << x << ", " << "y= " << y << ", " << "z= " << z << endl;
-	}
+	//void  info()const {
+	//	//Geometric_figures::info();
+	//	cout << "x= " << x << ", " << "y= " << y << ", " << "z= " << z << endl;
+	//}
 	//double perimeter() const {//периметр
 	//	return r * r * M_PI;
 	//}
@@ -165,16 +174,23 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	
 	Circle cirkle;
-	cout<<cirkle.perimeter(12)<<endl;
+	cirkle.set_r(8);
+	cout<<"Периметр круга с радиусом " << cirkle.get_r() << "= " <<cirkle.perimeter() << endl;
+	cout<<"Площадь  круга с радиусом " << cirkle.get_r() << "= " <<cirkle.square() << endl;
+
 	Square_figure kvadrat;
-	cout << kvadrat.perimeter(12) << endl;
+	kvadrat.set_width(7);
+	cout <<"Периметр квадрата со стороной :" << kvadrat.get_width() << "= " << kvadrat.perimeter() << endl;
+	cout <<"Площадь  квадрата со стороной :" << kvadrat.get_width() << "= " << kvadrat.square() << endl;
+
+	Rectangle pryamougolnik;
+	pryamougolnik.set_two_param(6, 7);
+	cout <<"Периметр прямоугольника со сторонами :" << pryamougolnik.get_height() << ", " << pryamougolnik.get_width() << "= " << pryamougolnik.perimeter() << endl;
+	cout <<"Площадь  прямоугольника со сторонами :" << pryamougolnik.get_height() << ", " << pryamougolnik.get_width() << "= " << pryamougolnik.square() << endl;
 
 
-	Geometric_figures *xz;
-	cout << &xz << endl;
 
-	/*Rectangle pryamougolnik;
-	cout<<pryamougolnik.perimeter(12,13)<<endl;*/
+
 
 	return 1;
 }
